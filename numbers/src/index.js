db2079e14d80ee4e26ab1b4d9aae7fb25002fcf9
Margozae <59208589+Margozae@users.noreply.github.com>
2020-03-12 02:29:22 +0200

    Add files via upload

--- numbers/src/index.js
+++ numbers/src/index.js
@@ -1,25 +1,20 @@
+'use strict';
 
-const mainDiv = document.createElement('div');
-document.body.append(mainDiv);
-mainDiv.style.display = 'flex';
-for(let i=0; i < 5; i++) {  // eslint-disable-line 
-    const div = document.createElement('div');
-    mainDiv.append(div);
+const container = document.querySelector('.container');
+container.style.display = 'flex';
+const square = document.querySelectorAll('.square');
+
+for(let i = 0; i < square.length; i++) {
     let value = 0;
     const divValue = document.createElement('div');
-    div.append(divValue);
+    divValue.style.marginBottom = '15px';
+    square[i].append(divValue);
     divValue.append(value);
-    div.style.height = '80px';
-    div.style.width = '80px';
-    div.style.marginRight = '50px';
-    div.style.background = 'pink';
-    div.style.border = '1px solid #000000';
-    div.style.textAlign = 'center';
     const btnPlus = document.createElement('button');
-    div.append(btnPlus);
+    square[i].append(btnPlus);
     btnPlus.append('+');
     const btnMinus = document.createElement('button');
-    div.append(btnMinus);
+    square[i].append(btnMinus);
     btnMinus.append('-');
     btnPlus.addEventListener('click', ( () => {
         divValue.textContent = ++value;
